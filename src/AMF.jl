d6ddd9e35b099e2706077998401ac4f9b7a4142b
Gaurav Arya <gauravarya272@gmail.com>
2022-12-15 16:10:46 -0500

    Add dependencies

--- src/AMF.jl
+++ src/AMF.jl
@@ -1,5 +1,18 @@
 module AMF
 
-# Write your package code here.
+using SciMLOperators
+using OrdinaryDiffEq
+using LinearAlgebra
+using Plots
+using MethodOfLines
+using ModelingToolkit
+using DomainSets
+using LinearSolve
+using SparseArrays
+
+include("method_of_lines.jl")
+include("finite_differences.jl")
+
+export solve2d, FiniteDifferenceMethod, MethodOfLinesMethod
 
 end
